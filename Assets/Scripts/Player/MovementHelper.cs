eed1374ccd9140baf01ede327e6bcbd29540d656
pmartinsesa <pmartinsesa@gmail.com>
2024-08-13 14:45:41 -0300

    chore: add basic movement

---
+++ Assets/Scripts/Player/MovementHelper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class MovementHelper : MonoBehaviour
+    {
+        [Header("Lerp Settings")]
+        public float lerpTime;
+        public Transform target;
+
+        private void Update()
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, lerpTime * Time.deltaTime);
+        }
+    }
+}
