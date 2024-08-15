6850daf3048c15005b496040fdaf67ad2ffb5d72
pmartinsesa <pmartinsesa@gmail.com>
2024-08-15 18:11:55 -0300

    chore: add animation

---
+++ Assets/Scripts/Player/AnimationHelper.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class AnimationHelper : MonoBehaviour
+    {
+        [Header("Animator Settings")]
+        public Animator animator;
+        public string runTrigger;
+        public string deadTrigger;
+        public string idleTrigger;
+
+        [Header("Animation Settings")]
+        public Animation running;
+
+        public void onRun()
+        {
+            animator.SetTrigger(runTrigger);
+        }
+
+        public void onIdle()
+        {
+            animator.ResetTrigger(runTrigger);
+            animator.SetTrigger(idleTrigger);
+        }
+
+        public void onDead()
+        {
+            animator.ResetTrigger(runTrigger);
+            animator.SetTrigger(deadTrigger);
+        }
+    }
+}
